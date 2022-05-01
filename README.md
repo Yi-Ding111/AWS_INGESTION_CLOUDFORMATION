ebd551acbd4f94c203c4ffb0acb032f6caa4d2ab
Yi Ding <dydifferent@gmail.com>
2022-05-02 05:31:05 +1000

    comm

---
+++ README.md
@@ -0,0 +1,29 @@
+## WEATHER API INGESTION
+---
+
+### On AWS services: S3, EventBridge Rule, Lambda, IAM Role, CloudFormation
+
+* API source: from https://weatherstack.com/ . 
+
+    The API endpoint chooses from Current Weather: Get current weather data.
+
+* This project will extract weather data using AWS Lambda on schedule at a regular rate (each 12 hours), and load data into corresponding S3 Bucket. The following is all steps to set up the process.
+    
+    1. Lambda function needs REQUESTS package as a layer. Load [python.zip](python.zip) into one existing S3 Bucket.
+
+    2. Run [Cloudformation.yaml](CloudFormation.yaml) to set up all AWS configrations.
+
+    3. Give Cloudformation Template Parameters:
+        
+        * LambdaFunctionName: Give the Lambda a name (default)
+        * LambdaRole: Give the Lambda Execution Role (IAM Role) a name (default)
+        * LayerVersionBucket: Give the Bucket a name where the Lambda layer zip stored
+        * EventBridgeSchedule: Give the event rule a name (default)
+        * S3BucketName: Give the S3 Bucket a name
+    
+    4. MENTION: Users need to change DESTINATION BUCKET NAME in the Lambda function.
+
+
+---
+Creator: YI DING
+
---
+++ ingestion_lambda.py
@@ -0,0 +1,39 @@
+import requests 
+import boto3
+import json
+
+s3=boto3.client('s3')
+
+#weatherstack API
+api_key='5aa684621a44fa49f066ff57b930b3eb'
+endpoint = 'http://api.weatherstack.com/current'
+
+params = {
+        'access_key': api_key,
+        'query': 'Sydney'
+        }
+
+def lambda_handler(event, context):
+    #get today's weather info
+    api_result = requests.get(endpoint, params)
+    api_response = api_result.json()
+
+    #reconstruct data
+    payload={
+        'city':api_response["location"]["name"],
+        'country':api_response["location"]["country"],
+        'ingest_time':api_response["location"]["localtime"],
+        'observation_time':api_response["current"]["observation_time"],
+        'temperature':api_response["current"]["temperature"],
+        'weather_descriptions':api_response["current"]["weather_descriptions"],
+        'wind_speed':api_response["current"]["wind_speed"],
+        'wind_degree':api_response["current"]["wind_degree"],
+        'wind_dir':api_response["current"]["wind_dir"],
+        'cloudcover':api_response["current"]["cloudcover"]
+    }
+    
+    #load data into s3 bucket
+    data=json.dumps(payload)
+    filename=api_response["location"]["localtime"]+".json"
+    
+    s3.put_object(Bucket="<! DESTINATION_BUCKET_NANE >",Key=filename,Body=data)
